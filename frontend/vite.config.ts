22b31cd2bb9c8b19a43c385674e4a1f8470bf456
maximilianKalff <maximilian.kalff@student.hpi.uni-potsdam.de>
2026-01-15 16:13:05 +0100

    gh pages ready

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,4 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  // Setze hier den Repository-Namen ein, z.B. base: '/coach_os/'
+  base: '/coach_max/',
 });
